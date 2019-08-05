5716262ea8f70563ea5dd465de4b293db7534df5
everpalm <everpalm@ms58.url.com.tw>
2019-08-05 13:12:29 +0800

    CTJ1908051312 Improve algorithm

--- test_combination1/test_combination1.py
+++ test_combination1/test_combination1.py
@@ -1,61 +1,19 @@
 # This is test version
 # 7 elements stnad for 7 items of different price, choose two of them from these 7 items with 10 dollars
-def append_list_10(target_x, input_y):
-    if input_y <= 10:
-        dummy = target_x.append(input_y)
-        return True
-    else:
-        return False
-
-def append_sample(target_list, append_list, b_num, z):
-    y = append_list[0] + append_list[b_num]
-    if append_list_10(z,y):
-        dummy = target_list.append([append_list[0], append_list[b_num]])
-    return target_list
-
-if __name__ == "__main__":
-    a = [3,6,8,7,9,5,2]
-    z = []
-    sample = []
-
-    a_len = len(a)
-
-#print(a_len)
-#x1 = a[0:a_len]
-#x2 = a[1:a_len]
-#x3 = a[2:a_len]
-#x4 = a[3:a_len]
-#x5 = a[4:a_len]
-    for a_num in range(a_len-1):
-#for a_num in range(0,a_len-1,1):
-        x = a[a_num:a_len]
-        x_len = len(x)
-        #print(x)
-        for b_num in range(1, x_len, 1):
-            #y = x[0] + x[b_num]
-            #print(y)
-            #if y <= 10:
-            #    dummy = z.append(y)
-            #if append_list_10(z,y):
-            #    dummy = sample.append([x[0], x[b_num]])
-            #else:
-            #    continue
-            #sample = append_sample(sample, x, b_num, z, y)
-            sample = append_sample(sample, x, b_num, z)
-
-    z_len = len(z) - 1
-    #print(z_len)
-    index_m = []
-
-    for z_num in range(z_len):
-        for y_num in range(z_len, z_num, -1):
-            if z[y_num] < z[y_num - 1]:
-                z[y_num], z[y_num - 1] = z[y_num - 1], z[y_num]
-                sample[y_num], sample[y_num - 1] = sample[y_num - 1], sample[y_num]
+# Brute force
+class Solution():
+    def TwoSumBrute(self, price_list, target_price):
+        print("price_list = ", price_list, ", target_price = ", target_price)
+        for anchor_count in range(len(price_list)):
+            for shift_count in range(anchor_count+1, len(price_list),1):
+                if price_list[anchor_count] + price_list[shift_count] == 10:
+                    return [anchor_count, shift_count]
+    
+    def TwoSumImprove(self, price_list, target_price):
+        print("price_list = ", price_list, ", target_price = ", target_price)
+        complement_list = {}
+        for anchor_count in range(len(price_list)):
+            if (target_price - price_list[anchor_count]) not in (complement_list):
+                complement_list[price_list[anchor_count]] = anchor_count
             else:
-                #dummy = index_m.insert(0,y_num)
-                #print(sample[y_num])
-                continue
-    sample_len = len(sample)
-    print(sample[sample_len - 1])
-    print(sample)
+                return [complement_list[target_price - price_list[anchor_count]], anchor_count]
--- test_combination1/test_module.py
+++ test_combination1/test_module.py
@@ -1,19 +1,23 @@
 import unittest
 from unittest import mock
-from test_combination1 import append_list_10, append_sample
+from test_combination1 import Solution as sl
 
 class test_module(unittest.TestCase):
     """description of class"""
-    def test_append_list_10(self):
-        self.assertEqual(append_list_10([],10), True)
-        self.assertEqual(append_list_10([],11), False)
+    # This is test version
+    # 7 elements stnad for 7 items of different price, choose two of them from these 7 items with 10 dollars
+
+    def test_two_sum(self):
+        self.assertEqual(sl().TwoSumBrute([3,4,5,6,7,8,9], 10), [0,4])
     
-    @mock.patch('test_combination1.append_list_10')
-    def test_append_sample(self, mock_append_list_10):
-        mock_append_list_10.return_value = True
-        self.assertEqual(append_sample([],[1,2],1,1), [[1,2]])
-        mock_append_list_10.return_value = False
-        self.assertEqual(append_sample([],[1,2],1,1), [])
+    def test_two_sum_improve(self):
+        self.assertEqual(sl().TwoSumImprove([3,4,5,6,7,8,9], 10), [1,3])
+    #@mock.patch('test_combination1.append_list_10')
+    #def test_append_sample(self, mock_append_list_10):
+     #   mock_append_list_10.return_value = True
+     #   self.assertEqual(append_sample([],[1,2],1,1), [[1,2]])
+     #   mock_append_list_10.return_value = False
+     #   self.assertEqual(append_sample([],[1,2],1,1), [])
 
 if __name__ == "__main__":
     unittest.TextTestRunner(verbosity=3).run(unittest.TestLoader().loadTestsFromTestCase(test_module))
